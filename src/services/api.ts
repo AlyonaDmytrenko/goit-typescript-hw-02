db20a21034885f816869281ed62634ae27ca0694
AlyonaDmytrenko <143014363+AlyonaDmytrenko@users.noreply.github.com>
2025-06-23 12:12:12 +0300

    error corrected

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,27 @@
+// src/services/api.ts
 import axios from "axios";
+import { Photo } from "../components/App/App.types";
 
-export const requestPhoto = async () => {
-  const { data } = await axios.get(
-    "https://api.unsplash.com/photos/?client_id=vA8gpQ6KTBVEHn7Ww1-SyYtI9u5qW9n1XEea-EcDvQg"
+const ACCESS_KEY = "vA8gpQ6KTBVEHn7Ww1-SyYtI9u5qW9n1XEea-EcDvQg";
+const BASE_URL = "https://api.unsplash.com";
+
+export const requestPhotoByQuery = async (
+  query: string,
+  page: number = 1
+): Promise<{ results: Photo[]; total: number; total_pages: number }> => {
+  const response = await axios.get<{
+    results: Photo[];
+    total: number;
+    total_pages: number;
+  }>(
+    `${BASE_URL}/search/photos/?client_id=${ACCESS_KEY}&query=${query}&page=${page}`
   );
-  return data;
+  return response.data;
 };
 
-export const requestPhotoByQuery = async (query = "") => {
-  const { data } = await axios.get(
-    `https://api.unsplash.com/search/photos/?client_id=vA8gpQ6KTBVEHn7Ww1-SyYtI9u5qW9n1XEea-EcDvQg&query=${query}`
+export const requestPhotoById = async (id: string): Promise<Photo> => {
+  const response = await axios.get<Photo>(
+    `${BASE_URL}/photos/${id}?client_id=${ACCESS_KEY}`
   );
-  return data;
+  return response.data;
 };
